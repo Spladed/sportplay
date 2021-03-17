ecfee59b86561827f8a97795f80db16be4eb0150
Spladed <1310140596@qq.com>
2021-03-17 16:29:35 +0800

    完成添加用户的功能

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -21,52 +21,72 @@
                 <el-col :span="4">
                     <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
                 </el-col>
-
-                <!--用户列表 border边框 stripe隔行变色-->
-                <el-table :data="userList" border stripe>
-                    <!--索引列-->
-                    <el-table-column type="index"></el-table-column>
-                    <el-table-column label="用户名" prop="username"></el-table-column>
-                    <el-table-column label="邮箱" prop="email"></el-table-column>
-                    <el-table-column label="密码" prop="password"></el-table-column>
-                    <el-table-column label="角色" prop="role"></el-table-column>
-                    <el-table-column label="状态" prop="state">
-                        <!--作用域插槽-->
-                        <template slot-scope="scope">
-                            <!-- scope.row 每一行封存的数据-->
-                            <el-switch v-model="scope.row.state" @change="userStateChange(scope.row)"></el-switch>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="操作">
-                        <template slot-scope="scope">
-                            <!-- 修改 -->
-                            <el-button type="primary" icon="el-icon-edit" size="mini"
-                                       @click="showEditDialog(scope.row.id)"></el-button>
-                            <!-- 删除 -->
-                            <el-button type="danger" icon="el-icon-delete" size="mini"
-                                       @click="deleteUser(scope.row.id)"></el-button>
-                            <!-- 权限 -->
-                            <el-tooltip effect="dark" content="分配权限" placement="top-start" :enterable="false">
-                                <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
-                            </el-tooltip>
-                        </template>
-                    </el-table-column>
-                </el-table>
-
-                <!--分页组件 size-change每页最大数发生变化时触发 current-change当前页数变化时触发 layout功能组件-->
-                <div>
-                    <el-pagination
-                            @size-change="handleSizeChange"
-                            @current-change="handleCurrentChange"
-                            :current-page="queryInfo.pageNum"
-                            :page-sizes="[1, 2, 5, 10]"
-                            :page-size="queryInfo.pageSize"
-                            layout="total, sizes, prev, pager, next, jumper"
-                            :total="total">
-                    </el-pagination>
-                </div>
             </el-row>
+
+            <!--用户列表 border边框 stripe隔行变色-->
+            <el-table :data="userList" border stripe>
+                <!--索引列-->
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="用户名" prop="username"></el-table-column>
+                <el-table-column label="邮箱" prop="email"></el-table-column>
+                <el-table-column label="密码" prop="password"></el-table-column>
+                <el-table-column label="角色" prop="role"></el-table-column>
+                <el-table-column label="状态" prop="state">
+                    <!--作用域插槽-->
+                    <template slot-scope="scope">
+                        <!-- scope.row 每一行封存的数据-->
+                        <el-switch v-model="scope.row.state" @change="userStateChange(scope.row)"></el-switch>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="操作">
+                    <template slot-scope="scope">
+                        <!-- 修改 -->
+                        <el-button type="primary" icon="el-icon-edit" size="mini"
+                                   @click="showEditDialog(scope.row.id)"></el-button>
+                        <!-- 删除 -->
+                        <el-button type="danger" icon="el-icon-delete" size="mini"
+                                   @click="deleteUser(scope.row.id)"></el-button>
+                        <!-- 权限 -->
+                        <el-tooltip effect="dark" content="分配权限" placement="top-start" :enterable="false">
+                            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+                        </el-tooltip>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+            <!--分页组件 size-change每页最大数发生变化时触发 current-change当前页数变化时触发 layout功能组件-->
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="queryInfo.pageNum"
+                    :page-sizes="[1, 2, 5, 10]"
+                    :page-size="queryInfo.pageSize"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="total">
+            </el-pagination>
         </el-card>
+        <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
+            <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+                <!-- 用户名 -->
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="addForm.username"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="addForm.password"></el-input>
+                </el-form-item>
+                <!-- 邮箱 -->
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="addForm.email"></el-input>
+                </el-form-item>
+            </el-form>
+            <!-- 内容底部区域 -->
+            <span slot="footer" class="dialog-footer">
+                    <el-button @click="addDialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="addUser">确 定</el-button>
+                </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -85,6 +105,27 @@ export default {
             },
             userList: [],    //用户列表
             total: 0,    //总记录数
+            addDialogVisible: false,     //对话框显示或隐藏的状态
+            // 添加表单信息
+            addForm: {
+                username: '',
+                password: '',
+                email: '',
+            },
+            addFormRules: {
+                username: [
+                    {required: true, message: '请输入用户名', trigger: 'blur'},
+                    {min: 5, max: 8, message: '长度在 5 到 8 个字符', trigger: 'blur'}
+                ],
+                password: [
+                    {required: true, message: "请输入密码", trigger: "blur"},
+                    {min: 6, max: 8, message: "长度在 6 到 8 个字符", trigger: "blur"}
+                ],
+                email: [
+                    {required: true, message: "请输入邮箱", trigger: "blur"},
+                    {min: 5, max: 15, message: "请输入正确邮箱地址", trigger: "blur"}
+                ],
+            }
         }
     },
     methods: {
@@ -104,6 +145,7 @@ export default {
             this.queryInfo.pageNum = newPage;
             this.getUserList();
         },
+        // 修改用户状态
         async userStateChange(userInfo) {
             const {data: res} = await this.$http.put(`userstate?id=${userInfo.id}&state=${userInfo.state}`);
             if (res != "success") {
@@ -111,6 +153,23 @@ export default {
                 return this.$message.error("操作失败");
             }
             this.$message.success("操作成功");
+        },
+        // 监听添加用户
+        addDialogClosed() {
+            this.$refs.addFormRef.resetFields();
+        },
+        // 添加用户
+        addUser() {
+            this.$refs.addFormRef.validate(async valid => {
+                if (!valid) return;
+                const {data: res} = await this.$http.post("adduser", this.addForm);
+                if (res != "success") {
+                    return this.$message.error("操作失败");
+                }
+                this.$message.success("操作成功");
+                this.addDialogVisible = false;
+                this.getUserList();
+            });
         }
     },
 }
