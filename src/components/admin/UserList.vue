03035a34b2c80cd9763f7fd213913d2cbb3b1c59
Spladed <1310140596@qq.com>
2021-03-17 16:47:39 +0800

    完成删除用户的功能

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -170,7 +170,24 @@ export default {
                 this.addDialogVisible = false;
                 this.getUserList();
             });
-        }
+        },
+        //根据主键删除用户
+        async deleteUser(id) {
+            const confirmResult = await this.$confirm('此操作将永久删除用户，是否继续？', '提示', {
+                confirmButtonText:'确定',
+                cancelButtonText:'取消',
+                type:"warning"
+            }).catch(err => err)
+            if(confirmResult!='confirm'){
+                return this.$message.info('已取消删除');
+            }
+            const {data:res}=await this.$http.delete("deleteuser?id="+id);
+            if(res!='success'){
+                return this.$message.error('删除失败');
+            }
+            this.$message.success('删除成功');
+            this.getUserList();
+        },
     },
 }
 </script>
