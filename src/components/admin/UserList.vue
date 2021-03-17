1c0e5dd17b58737e30ae892d9c02cddecd56eb7f
Spladed <1310140596@qq.com>
2021-03-17 15:51:42 +0800

    完成搜索功能

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -13,7 +13,7 @@
                 <!--列宽-->
                 <el-col :span="10">
                     <!--搜索区域-->
-                    <el-input placeholder="请输入搜索内容">
+                    <el-input placeholder="请输入搜索内容" v-model="queryInfo.query" clearable @clear="getUserList">
                         <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
                     </el-input>
                 </el-col>
